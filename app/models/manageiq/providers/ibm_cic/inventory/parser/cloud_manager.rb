4a602dc7d9196318c1d42f26089cbe0fae0110cd
Adam Grare <adam@grare.com>
2022-04-20 09:57:10 -0400

    Merge pull request #12 from lidanbj/ibm/cic-provider
    Get cpu_speed from cic to set into hardware.
    
    (cherry picked from commit 0c6e7205bf916f35d0cdd45e322a4821513da138)

--- app/models/manageiq/providers/ibm_cic/inventory/parser/cloud_manager.rb
+++ app/models/manageiq/providers/ibm_cic/inventory/parser/cloud_manager.rb
@@ -1,2 +1,13 @@
 class ManageIQ::Providers::IbmCic::Inventory::Parser::CloudManager < ManageIQ::Providers::Openstack::Inventory::Parser::CloudManager
+  def parse_vm(vm, hosts)
+    super
+    server = persister.vms.find_or_build(vm.id.to_s)
+    hardware = persister.hardwares.find_or_build(server)
+    # In openstack provider hardware cpu_speed is retrieved from the parent_host.
+    # hardware.cpu_speed = parent_host.try(:hardware).try(:cpu_speed)
+    # In cic provider, cpu_speed is from server details and set to hardware cpu_speed
+    if vm.attributes.key?("cpu_speed")
+      hardware.cpu_speed = vm.attributes["cpu_speed"]
+    end
+  end
 end

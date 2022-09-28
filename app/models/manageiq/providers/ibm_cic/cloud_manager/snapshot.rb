ac91ca5365c7b5910c9f2f7c26921b72c4b529c0
Adam Grare <adam@grare.com>
2022-09-28 10:41:55 -0400

    Subclass Snapshot under IbmCic::CloudManager

---
+++ app/models/manageiq/providers/ibm_cic/cloud_manager/snapshot.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Openstack::CloudManager::Snapshot.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmCic::CloudManager::Snapshot < ManageIQ::Providers::Openstack::CloudManager::Snapshot
+end

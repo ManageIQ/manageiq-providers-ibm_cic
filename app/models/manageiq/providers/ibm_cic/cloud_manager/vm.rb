8bfe16db42a52c4d3a723acfca0858fcf2311028
Adam Grare <adam@grare.com>
2023-01-04 16:23:36 -0500

    Add `supports :capture` to supported subclasses

--- app/models/manageiq/providers/ibm_cic/cloud_manager/vm.rb
+++ app/models/manageiq/providers/ibm_cic/cloud_manager/vm.rb
@@ -1,4 +1,5 @@
 ManageIQ::Providers::Openstack::CloudManager::Vm.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::IbmCic::CloudManager::Vm < ManageIQ::Providers::Openstack::CloudManager::Vm
+  supports :capture
 end

868135f12791b303e4cff6ec001f6ea78fd33f39
Joe Rafaniello <jrafanie@gmail.com>
2023-04-21 17:00:43 -0400

    Move gem name loader to proper namespaced location.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ end
 Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
 Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
-require "manageiq-providers-ibm_cic"
+require "manageiq/providers/ibm_cic"
 
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']

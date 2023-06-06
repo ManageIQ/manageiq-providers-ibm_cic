88b274bb06fccf61c55d9d51165fb57fae070721
Adam Grare <adam@grare.com>
2023-06-06 10:41:24 -0400

    Fix expired_at causing VCRs to fail authentication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,19 @@ Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
 require "manageiq/providers/ibm_cic"
 
+def fix_token_expires_at(interaction)
+  data = JSON.parse(interaction.response.body)
+  data["token"]["expires_at"] = "9999-12-31T23:59:59.999999Z"
+  interaction.response.body = data.to_json.force_encoding('ASCII-8BIT')
+end
+
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = ManageIQ::Providers::IbmCic::Engine.root.join("spec/vcr_cassettes")
+
+  config.before_record do |interaction|
+    fix_token_expires_at(interaction) if interaction.request.uri.end_with?("v3/auth/tokens")
+  end
 
   secrets = Rails.application.secrets
   secrets.ibm_cic.each_key do |secret|

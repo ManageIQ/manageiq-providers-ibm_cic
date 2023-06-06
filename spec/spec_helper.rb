a338113cc3f5f4a33207b058f1608adb27fbdc7f
Adam Grare <adam@grare.com>
2023-06-06 15:09:27 -0400

    Use common fix_token_expires_at

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,22 +5,15 @@ end
 
 Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
 Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
+Dir[ManageIQ::Providers::Openstack::Engine.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 require "manageiq/providers/ibm_cic"
-
-def fix_token_expires_at(interaction)
-  data = JSON.parse(interaction.response.body)
-  data["token"]["expires_at"] = "9999-12-31T23:59:59.999999Z"
-  interaction.response.body = data.to_json.force_encoding('ASCII-8BIT')
-end
 
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = ManageIQ::Providers::IbmCic::Engine.root.join("spec/vcr_cassettes")
 
-  config.before_record do |interaction|
-    fix_token_expires_at(interaction) if interaction.request.uri.end_with?("v3/auth/tokens")
-  end
+  fix_token_expires_at(config)
 
   secrets = Rails.application.secrets
   secrets.ibm_cic.each_key do |secret|

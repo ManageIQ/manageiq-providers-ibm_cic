153c32357a7f5d9da7999d87c1370506d678ceef
Nasar Khan <nasarak98@gmail.com>
2024-10-31 11:21:47 -0400

    delegate child manager refresh_ems to parent cloud manager

--- app/models/manageiq/providers/ibm_cic/storage_manager/cinder_manager.rb
+++ app/models/manageiq/providers/ibm_cic/storage_manager/cinder_manager.rb
@@ -1,6 +1,10 @@
 ManageIQ::Providers::Openstack::StorageManager::CinderManager.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::IbmCic::StorageManager::CinderManager < ManageIQ::Providers::Openstack::StorageManager::CinderManager
+  class << self
+    delegate :refresh_ems, :to => ManageIQ::Providers::IbmCic::CloudManager
+  end
+
   def self.ems_type
     @ems_type ||= "ibm_cic_cinder".freeze
   end
